3851b8bb78cd74a9fe6abdd5833ec017b98017a0
Masha Burdo <masha.burdo@gmail.com>
2022-05-17 20:36:07 +0300

    first lesson

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    price = models.IntegerField()
