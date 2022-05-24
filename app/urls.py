aedb07a50d8f03b6001b0e96d4ae0d6a13b7699a
Maria Burdo <masha.burdo@gmail.com>
2022-05-24 19:06:00 +0300

    lesson 3

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('all_books/', views.all_books, name='all_books')
+    path('all_books/', views.all_books, name='all_books'),
+    path('book/<int:book_id>/', views.book_detail, name='book'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from . import models
 
 
@@ -13,3 +14,8 @@ def all_books(request):
         'books': books,
     }
     return render(request, 'all_books.html', {'context': context})
+
+
+def book_detail(request, book_id):
+    book = get_object_or_404(models.Book, id=book_id)
+    return HttpResponse(book.name)
