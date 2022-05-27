1fa11eec159f4b25d754253648e7c22886bc724b
Maria Burdo <masha.burdo@gmail.com>
2022-05-27 19:21:33 +0300

    lesson 3 hometask

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('all_books/', views.all_books, name='all_books'),
+    path('all_reviews/', views.all_reviews, name='all_reviews'),
     path('book/<int:book_id>/', views.book_detail, name='book'),
 ]
--- app/views.py
+++ app/views.py
@@ -16,6 +16,14 @@ def all_books(request):
     return render(request, 'all_books.html', {'context': context})
 
 
+def all_reviews(request):
+    reviews = models.Review.objects.all()
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, 'all_reviews.html', {'context': context})
+
+
 def book_detail(request, book_id):
     book = get_object_or_404(models.Book, id=book_id)
     context = {
