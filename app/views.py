a76d8c8299fe36b1053974b87850352ed94a06b8
Maria Burdo <masha.burdo@gmail.com>
2022-05-31 19:52:56 +0300

    lesson 4

--- app/views.py
+++ app/views.py
@@ -56,8 +56,6 @@ def login_view(request):
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            # username = request.POST['username']
-            # password = request.POST['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
